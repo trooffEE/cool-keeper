a74e9d50a4c742da77f934f430187c56f4a3f8a6
Vadim <spicyn.v2001@gmail.com>
2021-09-05 11:44:08 +0700

    (+) basic webpack config, tsx files

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = (props) => {
+  return <div>Webpack is cool!</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const { resolve, join } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const resolvePath = (p) => resolve(__dirname, p)
+
+const config = {
+  mode: isProd ? 'production' : 'development',
+  entry: {
+    index: './src/index.tsx',
+  },
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    modules: [join(__dirname, 'src'), 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@components': resolvePath('./src/components'),
+      '@assets': resolvePath('./src/assets'),
+      '@services': resolvePath('./src/services'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ]
+}
+
+if (isProd) {
+  config.optimization = {
+    minimizer: [new TerserWebpackPlugin()],
+  };
+} else {
+  config.devServer = {
+    port: 8080,
+    hot: true,
+    compress: true,
+  };
+}
+
+module.exports = config
